deddf02eb01e97e5f4c162b9ee5ab7216c7e2a7d
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-10-03 10:57:08 +0200

    vätte 2

--- app.py
+++ app.py
@@ -6,19 +6,24 @@ class Enemy:
         self.attack = attack
         self.name = name
     
-    def print_attack(self):
-        print(f"{self.name} attackerar dig och gör {self.attack} skada")
+    def attack(self, target):
+        print(f"{self.name} attackerar {target.name}")
+        target.take_damage(self.attack)
+
     
-    def take_damage(self):
-        self.health -= player['attack']
-        print(f"{self.name} tar {player['attack']} i skada och {self.health} hälsa kvar.")
-
-
+    def take_damage(self, damage):
+        self.health -= damage
+        print(f"{self.name} tar {damage} i skada och {self.health} hälsa kvar.")
 
     def print_status(self):
-        print(f"En {self.name} står framför dig och har {self.health} hälsa och {self.attack} attack.")
+        print(f"{self.name} står framför dig och har {self.health} hälsa och {self.attack} attack.")
 
-vätte = Enemy(10, 5, "Vätte")
-vätte.print_status()
-vätte.print_attack()
-vätte.take_damage()
+creature_1 = Enemy(10, 5, "Vätte")
+creature_2 = Enemy(25, 8, "Troll")
+
+creature_1.print_status()
+creature_2.print_status()
+
+creature_1.attack(creature_2.)
+creature_2.take_damage(creature_1.attack)
+creature_2.print_status()
