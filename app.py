3777a96585a721945a1769275903bed4586f9e39
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-10-08 13:42:47 +0200

    death

--- app.py
+++ app.py
@@ -6,14 +6,18 @@ class Enemy:
         self.attack = attack
         self.name = name
     
-    def attack(self, target):
+    def hurt(self, target):
         print(f"{self.name} attackerar {target.name}")
         target.take_damage(self.attack)
 
     
     def take_damage(self, damage):
         self.health -= damage
-        print(f"{self.name} tar {damage} i skada och {self.health} hälsa kvar.")
+        if self.health > 0:
+            print(f"{self.name} tar {damage} i skada och {self.health} hälsa kvar.")
+        else:
+            print(f"{self.name} tar {damage} i skada och dör")
+        
 
     def print_status(self):
         print(f"{self.name} står framför dig och har {self.health} hälsa och {self.attack} attack.")
@@ -21,9 +25,11 @@ class Enemy:
 creature_1 = Enemy(10, 5, "Vätte")
 creature_2 = Enemy(25, 8, "Troll")
 
-creature_1.print_status()
-creature_2.print_status()
+#creature_1.print_status()
+#creature_2.print_status()
 
-creature_1.attack(creature_2.)
-creature_2.take_damage(creature_1.attack)
-creature_2.print_status()
+while True:
+    creature_1.hurt(creature_2)
+    creature_1.print_status()
+    creature_2.print_status()
+    input("")
